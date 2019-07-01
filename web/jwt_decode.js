0d0f3b065fafa289dccb05d376758a6cd57ee44e
Nick Frichette <nick@frichetten.com>
2019-07-01 13:15:54 -0500

    Added some new features to jwt_decode.js

--- web/jwt_decode.js
+++ web/jwt_decode.js
@@ -1,10 +1,13 @@
-parseJwt = function(token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace('-', '+').replace('_', '/');
+parseJwt = function(token, section) {
+  var header = token.split('.')[section];
+  var base64 = header.replace('-', '+').replace('_', '/');
   var buf = new Buffer(base64, 'base64');
   return JSON.parse(buf);
 }
 
-var decoded = parseJwt(process.argv[2]);
+var header = parseJwt(process.argv[2], 0);
+var body = parseJwt(process.argv[2], 1);
 
-console.log(decoded);
+console.log(header);
+console.log(body);
+console.log(process.argv[2].split('.')[2]);
