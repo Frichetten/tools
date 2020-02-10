4d4262cae9aaad311f50f2c96d847751e70427ee
Nick Frichette <nick@frichetten.com>
2020-02-10 08:43:00 -0600

    Added an additional check for Kubernetes priv check

--- docker/kubernetes_check_internal_priv.sh
+++ docker/kubernetes_check_internal_priv.sh
@@ -3,3 +3,6 @@ TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 CACERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 curl -H "Authorization: Bearer $TOKEN" --cacert $CACERT $K8S/healthz
+
+# Example if you get kubectl into the container
+./kubectl --token $TOKEN --server $K8S get pods
