8fa135210b950eb030278ad5b355cbb0fea3c0df
Nick Frichette <nick@frichetten.com>
2020-06-13 14:19:35 -0500

    Created container mode for arsenal. arsenal.sh can now be launched either for a container or a host

--- arsenal.sh
+++ arsenal.sh
@@ -3,12 +3,25 @@
 # Intended OS: Kali Linux/Debian/Ubuntu
 # Description: A quick script to install some tools after 
 # the OS has been flashed.
+#
+# This script has two forms:
+#   1) Intended to run on a host machine. This can be a laptop/desktop/server/vm/etc.
+#   2) Intended to run from a container. 
+#
+# You can select which you'd like from the cli with either 'host' or 'container' as
+# a cli argument.
 
 # Root check
 if [ $EUID != 0 ]; then
     echo "[!] To install you must run as root."
     exit 1
 fi 
+
+# Param check
+if ([ "$#" != 1 ]) || ([ $1 != "host" ] && [ $1 != "container" ]); then
+    echo "[!] Usage: sudo ./arsenal.sh (host || container)"
+    exit 2
+fi
 
 echo "#########################################"
 echo "#                                       #"
@@ -23,15 +36,10 @@ echo "#########################################"
 read -p "[?] Are you sure you want to install the Arsenal? [Y/n]: " answer
 if [ $answer != "Y" ]; then
     echo "[!] Did not answer yes. Exiting."
+    exit 3
 fi
 
-echo "[+] Hardening OS"
-echo "################"
-
-echo "Hardening PTRACE scope = 3"
-sed -i '$ d' /etc/sysctl.d/10-ptrace.conf
-echo "kernel.yama.ptrace_scope = 3" >> /etc/sysctl.d/10-ptrace.conf
-
+# Update OS ###############################################
 echo "[+] Updating the OS"
 apt update
 apt upgrade -y
@@ -39,9 +47,126 @@ apt dist-upgrade -y
 
 echo "[+] Installing Tools"
 echo "####################"
+echo ""
 
-echo "[+] Installing i3wm"
-apt install -y i3
+if [ $1 == 'host' ]; then
+    echo "[+] Installing Host Tools"
+    echo "#########################"
+    
+    echo "[+] Installing i3wm"
+    apt install -y i3
+
+    echo "[+] Install arandr"
+    apt install -y arandr
+
+    echo "[+] Install Rofi"
+    apt install -y rofi
+
+    echo "[+] Install Nitrogen"
+    apt install -y nitrogen
+
+    echo "[+] Install i3-gaps"
+    apt install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb \
+        libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev \
+        libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev \
+        libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev \
+        libxcb-xrm0 libxcb-xrm-dev autoconf
+    cd /tmp
+    git clone https://www.github.com/Airblader/i3 i3-gaps
+    cd /tmp/i3-gaps
+    autoreconf --force --install
+    rm -rf /tmp/i3-gaps/build
+    mkdir -p /tmp/i3-gaps/build
+    cd /tmp/i3-gaps/build
+    ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+    make
+    make install
+    cd /home/nick/tools
+
+    echo "[+] Install Scrot"
+    apt install -y scrot
+
+    echo "[+] Install imagemagick"
+    apt install -y imagemagick
+
+    echo "[+] Install feh"
+    apt install -y feh
+
+    echo "[+] Install dunst"
+    apt install -y dunst
+
+    echo "[+] Install xautolock"
+    apt install -y xautolock
+
+    echo "[+] Install compton"
+    apt install -y compton
+
+    echo "[+] Install docker"
+    apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee -a /etc/apt/sources.list.d/docker.list
+    apt update
+    apt install -y docker-ce docker-compose
+
+    echo "[+] Install light"
+    cd /tmp
+    git clone https://github.com/haikarainen/light.git
+    cd light
+    ./autogen.sh
+    ./configure && make
+    make install
+    cd /home/nick/tools
+
+    echo "[+] Install urxvt"
+    # xsel and xclip are needed for copy paste
+    apt install -y rxvt-unicode xsel xclip
+
+    echo "[+] Install xdotool"
+    # needed for i3 config
+    apt install -y xdotool
+
+    echo "[+] Install KVM/QEMU"
+    apt install -y qemu-kvm libvirt0 virt-manager bridge-utils virt-goodies
+
+    echo "[+] Install keepassxc"
+    apt install -y keepassxc
+
+	echo "[+] Install transmission"
+	apt install -y transmission
+
+	echo "[+] Install yubikey software"
+	apt install -y gnupg2 gnupg-agent dirmngr cryptsetup scdaemon pcscd secure-delete hopenpgp-tools yubikey-personalization
+
+	echo "[+] Install VSCode"
+	apt install -y software-properties-common apt-transport-https wget
+	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
+	add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+	apt update
+	apt install -y code
+
+	echo "[+] Install i3lock-fancy"
+	apt install -y i3lock-fancy
+
+	echo "[+] Install pavucontrol"
+	apt install -y pavucontrol
+
+	echo "[+] Install lxappearance"
+	apt install -y lxappearance
+
+	echo "[+] Download the latest Kali linux hopefully"
+	cd /home/nick/Downloads
+	curl https://cdimage.kali.org/current/ -s | grep installer-amd64.iso | cut -d "=" -f 5 | cut -d ">" -f 1 | sed 's/\"//g' | { read url; wget https://cdimage.kali.org/current/$url; }
+	cd /home/nick/tools
+
+	echo "[+] Install rdesktop"
+	apt install -y rdesktop
+
+	echo "[+] Install dbeaver"
+	snap install -y dbeaver-ce
+fi
+
+echo "[+] Installing Non-Host Tools"
+echo "#############################"
 
 echo "[+] Installing pip3"
 apt install -y python3-pip
@@ -49,71 +174,11 @@ apt install -y python3-pip
 echo "[+] Installing Vim"
 apt install -y vim
 
-echo "[+] Install PyWal"
-pip3 install --user pywal
-echo 'PATH="${PATH}:${HOME}/.local/bin"' >> /home/nick/.bashrc
-
 echo "[+] Install Ranger"
 apt install -y ranger
 
-echo "[+] Install arandr"
-apt install -y arandr
-
-echo "[+] Install Rofi"
-apt install -y rofi
-
-echo "[+] Install Nitrogen"
-apt install -y nitrogen
-
 echo "[+] Install Neofetch"
 apt install -y neofetch
-
-echo "[-] Remove Terminator"
-# Kali includes terminator by default and I hate it
-apt remove -y terminator
-
-echo "[+] Install i3-gaps"
-apt install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb \
-    libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev \
-    libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev \
-    libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev \
-    libxcb-xrm0 libxcb-xrm-dev autoconf
-cd /tmp
-git clone https://www.github.com/Airblader/i3 i3-gaps
-cd /tmp/i3-gaps
-autoreconf --force --install
-rm -rf /tmp/i3-gaps/build
-mkdir -p /tmp/i3-gaps/build
-cd /tmp/i3-gaps/build
-../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-make
-make install
-cd /home/nick/tools
-
-echo "[+] Install Scrot"
-apt install -y scrot
-
-echo "[+] Install imagemagick"
-apt install -y imagemagick
-
-echo "[+] Install feh"
-apt install -y feh
-
-echo "[+] Install dunst"
-apt install -y dunst
-
-echo "[+] Install xautolock"
-apt install -y xautolock
-
-echo "[+] Install compton"
-apt install -y compton
-
-echo "[+] Install docker"
-apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee -a /etc/apt/sources.list.d/docker.list
-apt update
-apt install -y docker-ce docker-compose
 
 echo "[+] Install aws cli"
 apt install -y awscli
@@ -134,28 +199,8 @@ cd /home/nick/scripts/pacu
 bash install.sh
 cd /home/nick/tools
 
-echo "[+] Install light"
-cd /tmp
-git clone https://github.com/haikarainen/light.git
-cd light
-./autogen.sh
-./configure && make
-make install
-cd /home/nick/tools
-
-echo "[+] Install urxvt"
-# xsel and xclip are needed for copy paste
-apt install -y rxvt-unicode xsel xclip
-
-echo "[+] Install xdotool"
-# needed for i3 config
-apt install -y xdotool
-
 echo "[+] Install htop"
 apt install -y htop
-
-echo "[+] Install KVM/QEMU"
-apt install -y qemu-kvm libvirt0 virt-manager bridge-utils virt-goodies
 
 echo "[+] Installing gobuster"
 apt install -y gobuster
@@ -168,9 +213,6 @@ cd /home/nick/tools
 echo "[+] Install net-tools"
 apt install -y net-tools
 
-echo "[+] Install keepassxc"
-apt install -y keepassxc
-
 echo "[+] Install smbclient"
 apt install -y smbclient
 
@@ -178,19 +220,6 @@ echo "[+] Install Linkfinder"
 cd /home/nick/scripts
 git clone https://github.com/GerbenJavado/LinkFinder.git
 cd /home/nick/tools
-
-echo "[+] Install transmission"
-apt install -y transmission
-
-echo "[+] Install yubikey software"
-apt install -y gnupg2 gnupg-agent dirmngr cryptsetup scdaemon pcscd secure-delete hopenpgp-tools yubikey-personalization
-
-echo "[+] Install VSCode"
-apt install -y software-properties-common apt-transport-https wget
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
-add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-apt update
-apt install -y code
 
 echo "[+] Install ssh (gives you openssh-server)"
 apt install -y ssh
@@ -210,14 +239,8 @@ apt install -y hashcat ocl-icd-opencl-dev
 echo "[+] Install sqlmap"
 apt install -y sqlmap
 
-echo "[+] Install i3lock-fancy"
-apt install -y i3lock-fancy
-
 echo "[+] Install tmux"
 apt install -y tmux
-
-echo "[+] Install cartography"
-pip3 install cartography
 
 echo "[+] Install jython"
 # Needed for some Burp extensions
@@ -234,12 +257,6 @@ apt install -y fonts-font-awesome
 echo "[+] Install nodejs"
 apt install -y nodejs
 
-#echo "[+] Install virtualbox"
-#apt install -y virtualbox
-
-echo "[+] Install pavucontrol"
-apt install -y pavucontrol
-
 echo "[+] Install updog"
 pip3 install updog
 
@@ -247,14 +264,6 @@ echo "[+] Install Chrome"
 cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install ./google-chrome-stable_current_amd64.deb
-cd /home/nick/tools
-
-echo "[+] Install lxappearance"
-apt install -y lxappearance
-
-echo "[+] Download the latest Kali linux hopefully"
-cd /home/nick/Downloads
-curl https://cdimage.kali.org/current/ -s | grep installer-amd64.iso | cut -d "=" -f 5 | cut -d ">" -f 1 | sed 's/\"//g' | { read url; wget https://cdimage.kali.org/current/$url; }
 cd /home/nick/tools
 
 echo "[+] Install Metasploit"
@@ -284,12 +293,6 @@ cd /home/nick/tools
 echo "[+] Install hashid"
 pip3 install hashid
 
-echo "[+] Install rdesktop"
-apt install -y rdesktop
-
-echo "[+] Install dbeaver"
-snap install -y dbeaver-ce
-
 echo "[+] Install resolvconf"
 apt install -y resolvconf
 mv /etc/resolv.conf /etc/resolv.conf.orig
@@ -298,23 +301,25 @@ ln -s /run/resolvconf/resolv.conf /etc/resolv.conf
 ##################
 echo "REQUIRES INTERACTION"
 
-echo "[+] Install Polybar"
-apt install -y cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev \
-    libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev \
-    libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm \
-    libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev \
-    libxcb-composite0-dev libjsoncpp-dev
-git clone https://github.com/jaagr/polybar.git
-mv polybar/ /tmp/
-cd /tmp/polybar
-./build.sh
-cd /home/nick/tools
+if [ $1 == 'host' ]; then
+	echo "[+] Install Polybar"
+	apt install -y cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev \
+		libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev \
+		libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm \
+		libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev \
+		libxcb-composite0-dev libjsoncpp-dev
+	git clone https://github.com/jaagr/polybar.git
+	mv polybar/ /tmp/
+	cd /tmp/polybar
+	./build.sh
+	cd /home/nick/tools
 
-echo "[+] Install wireshark"
-apt install -y wireshark-qt
+	echo "[+] Install wireshark"
+	apt install -y wireshark-qt
+fi
 
 echo "[+] Install kinit"
-apt install -y krb5-user
+apt install -y --force-yes krb5-user
 
 echo "[+] Fixing ownership of /scripts"
 cd /home/nick/
