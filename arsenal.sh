110737b87235f5bee0738ffaa2bd74d6ad524339
Nick Frichette <nick@frichetten.com>
2020-06-15 11:48:55 -0500

    Fixed dependencies for Polybar and i3-gaps

--- arsenal.sh
+++ arsenal.sh
@@ -75,11 +75,12 @@ if [ $1 == 'host' ]; then
     apt install -y nitrogen
 
     echo "[+] Install i3-gaps"
-    apt install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb \
-        libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev \
-        libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev \
-        libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev \
-        libxcb-xrm0 libxcb-xrm-dev autoconf
+    apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev \
+	libxcb-util0-dev libxcb-icccm4-dev libyajl-dev \
+	libstartup-notification0-dev libxcb-randr0-dev \
+	libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+	libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
+	autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
     cd /tmp
     git clone https://www.github.com/Airblader/i3 i3-gaps
     cd /tmp/i3-gaps
@@ -318,7 +319,7 @@ if [ $1 == 'host' ]; then
     echo "[+] Install Polybar"
     apt install -y cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev \
 	    libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev \
-	    libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm \
+	    libxcb-xkb-dev pkg-config python3-xcbgen xcb-proto libxcb-xrm-dev i3-wm \
 	    libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev \
 	    libxcb-composite0-dev libjsoncpp-dev
     git clone https://github.com/jaagr/polybar.git
